90418b08f8d27ded923966fc405c1ee0fe61e36f
Benjo <ben.ohman@gokstadakademiet.no>
2023-04-06 16:13:30 +0200

    scraping 3 newspapers

---
+++ index.js
@@ -0,0 +1,60 @@
+const PORT = 8790;
+const express = require("express");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+
+const app = express();
+
+const newspapers = [
+    {
+        name:"thetimes",
+        address:"https://www.thetimes.co.uk/environment/climate-change",
+        base:""
+    },
+    {
+        name:"guardian",
+        address:"https://www.theguardian.com/environment/climate-crisis",
+        base:"https://theguardian.com"
+    },
+    {
+        name:"telegraph",
+        address:"https://www.telegraph.co.uk/climate-change",
+        base:"https://www.telegraph.co.uk"
+    }
+
+];
+
+const articles = [];
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        $('a:contains("climate")',html).each(function() {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+
+            articles.push(
+                {
+                    title,
+                    url:newspaper.base + url,
+                    source:newspaper.name
+                });
+        })
+    })
+});
+
+app.get("/",(req,res) => {
+    res.json("Velkommen til klimaforandringsAPI");
+});
+
+app.get("/news",(req,res) => {
+    res.json(articles);
+});
+
+
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
